dd5dfedaf18fe529fac59b1c15513c8c1a66ce4b
GreatTomato <78492656+GreatTomato@users.noreply.github.com>
2022-01-11 21:05:43 +0200

    Update autoAnomaliesShower.lua

--- autoAnomaliesShower.lua
+++ autoAnomaliesShower.lua
@@ -1,6 +1,7 @@
+
 function piska()
     local ScreenGui = Instance.new("ScreenGui")
-    local TextLabel = Instance.new("TextLabel")
+    TextLabel = Instance.new("TextLabel")
 
     --Properties:
 
@@ -22,37 +23,44 @@ function piska()
 
     -- Scripts:
 
-    local function HDUW_fake_script() -- TextLabel.LocalScript 
-      local script = Instance.new('LocalScript', TextLabel)
-
-      wait(1)
-      local textLabel = script.Parent
-      local assus = ""
-      children = workspace.Objects.Anomalies:GetChildren()
-      local textSize = 0
-	  if #children == 0 then
-	  textLabel = "No anomalies here"
-	  textSize = 16
-	  else
-      for i = 1, #children do
-        local child = children[i]
-        assus = assus ..  " №" .. i .. ": ".. child.Name .. "\n"
-        textSize = textSize + 16
-      end
-	  end
-      --wait(10)
-      --textLabel.Text = " I ate a headgehog"
-      textLabel.Text = assus
-      textLabel.Size = UDim2.new(0, 150, 0,textSize)
-    end
-coroutine.wrap(HDUW_fake_script)()
+    --local function HDUW_fake_script() -- TextLabel.LocalScript 
+    function nona()
+		local script = Instance.new('LocalScript', TextLabel)
+		wait(1)
+		local textLabel = script.Parent
+		local assus = ""
+		children = workspace.Objects.Anomalies:GetChildren()
+		local textSize = 0
+		if #children == 0 then
+		textLabel = "No anomalies here"
+		textSize = 16
+		else
+		for i = 1, #children do
+			local child = children[i]
+			assus = assus ..  " №" .. i .. ": ".. child.Name .. "\n"
+			textSize = textSize + 16
+		end
+		end
+		--wait(10)
+		--textLabel.Text = " I ate a headgehog"
+		textLabel.Text = assus
+		textLabel.Size = UDim2.new(0, 150, 0,textSize)
+	end
+    --end
+coroutine.wrap(nona)()
 end
 
-game.Players.PlayerAdded:Connect(function(player)
-  local playerOnTeleport = player
-  player.OnTeleport:Connect(function(teleportState, placeId, spawnName)
-    pipiska()
-  end)
-end) --]]
 wait(1);
 piska()
+local UserInputService = game:GetService("UserInputService")
+game:GetService("UserInputService").InputEnded:connect(function(Input)
+    if Input.KeyCode == Enum.KeyCode.Space then
+        nona()
+    end
+end)
+
+--[[game:GetService('RunService').Heartbeat:Connect(function()
+  if UserInputService:IsKeyDown(Enum.KeyCode.Space) then
+    nona()
+  end
+end)]]
